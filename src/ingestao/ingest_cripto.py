f38b239379bc8cb3fb7b444661d5fd788cca1f79
Gabriel Perillo <gabrielperillo.dev@gmail.com>
2026-03-01 18:38:21 -0300

    looping cryptos in API

--- src/ingestao/ingest_cripto.py
+++ src/ingestao/ingest_cripto.py
@@ -4,43 +4,52 @@ import requests                         # requisição de API
 import json                             # trabalhar com Json
 from pathlib import Path                # manipular caminhos de arquivos e pastas 
 from datetime import datetime as dt     # pegar data atual
+import time 
 
 # Criando campos chave 
 
 apiKey = 'J8M9P0B2JCPHH1PM'
 
 # sourceCurrency
-srcCur = 'BTC'               # Moeda/Cripto origem que vou extrair os dados 
+#srcCur = 'BTC'               # Moeda/Cripto origem que vou extrair os dados 
 
 # convertedCurrency 
 convCur = 'USD'              # Moeda/Cripto para qual a origem será convertida
 
 repository = 'data_lake/raw'
 
+# Lista de cryptos 
+cryptos = ["BTC", "ETH", "SOL", "ADA", "XRP", "BNB", "DOT", "DOGE", "TRX", "LTC"]
+
+for srcCur in cryptos:
+
+    print('Fazendo nova requisição na API...')
+
+    # Fazendo requisição da API 
+    url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={srcCur}&to_currency={convCur}&apikey={apiKey}'
+    response = requests.get(url)
+
+    #print(response)
+
+    data = response.json()
 
 
-# Fazendo requisição da API 
+    # Salvando a response da API num arquivo json, na minha pasta local 
 
-url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={srcCur}&to_currency={convCur}&apikey={apiKey}'
-response = requests.get(url)
+    # currentDate
+    curDate = dt.today().strftime('%Y-%m-%d')
 
-#print(response)
+    output_dir = Path(repository) / srcCur 
+    output_dir.mkdir(parents=True, exist_ok=True)
 
-data = response.json()
+    file = output_dir / f"{srcCur}_{curDate}.json" 
 
+    with file.open("w") as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
 
-# Salvando a response da API num arquivo json, na minha pasta local 
+    print(f'file created: {file}')
 
-# currentDate
-curDate = dt.today().strftime('%Y-%m-%d')
-
-output_dir = Path(repository) / srcCur 
-output_dir.mkdir(parents=True, exist_ok=True)
-
-file = output_dir / f"{srcCur}_{curDate}.json" 
-
-with file.open("w") as f:
-    json.dump(data, f, indent=4, ensure_ascii=False)
+    time.sleep(12)
 
 #print(file.resolve())
 #print(data)
